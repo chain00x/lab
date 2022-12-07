03d372cbbeb0abed6b6c77b82c199de3d1484009
chain00x <90015694+chain00x@users.noreply.github.com>
2022-12-07 20:51:09 +0800

    Update domxss.php

--- domxss.php
+++ domxss.php
@@ -1,6 +1,16 @@
 <?php 
 $name=htmlentities($_GET['name'],ENT_QUOTES);
 $age=htmlentities($_GET['age'],ENT_QUOTES);
-
-echo "<script>var name='".$name."'\nvar age=".$age."</script>"
+$url=htmlentities($_GET['url'],ENT_QUOTES);
+$iframeurl=htmlentities($_GET['iframe'],ENT_QUOTES);
+echo "参数为name，age，a标签：url，iframe标签：iframe</br>";
+if($url){
+    echo "<a href=\"$url\">test</a>";
+}
+elseif($iframeurl){
+    echo "<iframe src=\"$iframeurl\">test</a>";
+}
+else{
+    echo "<script>var name='$name';var age='$age';</script>";
+}
 ?>
