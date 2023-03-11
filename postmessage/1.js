4a524240535061ebb2e8345b420b742aac5b3f52
chain00x <90015694+chain00x@users.noreply.github.com>
2023-03-11 22:08:42 +0800

    Create 1.js

---
+++ postmessage/1.js
@@ -0,0 +1,9 @@
+function we(img){
+    var div=document.createElement("div")
+    div.innerHTML = "<img src='"+img+"'>";
+}
+
+function we2(content){
+    var div=document.createElement("div")
+    div.innerHTML = content;
+}
